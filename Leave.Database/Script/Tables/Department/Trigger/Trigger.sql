913a96cdddd1687ae659c6d20043ee59e7564548
Piotr Walacik <piotr.walacik@hotmail.com>
2018-11-01 15:20:11 +0100

    modif

---
+++ Leave.Database/Script/Tables/Department/Trigger/Trigger.sql
@@ -0,0 +1,12 @@
+﻿CREATE TRIGGER [Trigger]
+	ON [dbo].[Department]
+	AFTER INSERT, UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		  
+	UPDATE X 
+	SET Inserted = GETDATE()
+	FROM [dbo].[Department] X
+	JOIN inserted i ON X.Id = i.Id
+END;
